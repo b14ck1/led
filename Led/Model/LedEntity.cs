3243b6b3d6eb00c4f48975217a3178731260ca85
bl4ck <bl4ck@posteo.net>
2018-10-22 20:50:54 +0200

    Added properties AllLedIDs

--- Led/Model/LedEntity.cs
+++ Led/Model/LedEntity.cs
@@ -20,6 +20,27 @@ namespace Led.Model
         [JsonProperty]
         public Dictionary<LedEntityView, ImageInfo> ImageInfos { get; set; }
 
+        public List<Utility.LedModelID> AllLedIDs
+        {
+            get
+            {
+                List<Utility.LedModelID> res = new List<Utility.LedModelID>();
+
+                foreach(var _ledBus in  LedBuses.Values)
+                {
+                    foreach(var _ledGroup in _ledBus.LedGroups)
+                    {
+                        for (ushort i = 0; i < _ledGroup.Leds.Count; i++)                        
+                        {
+                            res.Add(new Utility.LedModelID(_ledGroup.BusID, _ledGroup.PositionInBus, i));
+                        }
+                    }
+                }
+
+                return res;
+            }
+        }
+
         public Second[] Seconds { get; set; }
 
         public LedEntity()
