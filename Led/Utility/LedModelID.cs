dcd32eb5a9d88a182b12ef4f79f6632f8e0ceb06
bl4ck <bl4ck@posteo.net>
2018-10-22 20:48:47 +0200

    Added LedModelID Equals implementation

--- Led/Utility/LedModelID.cs
+++ Led/Utility/LedModelID.cs
@@ -19,5 +19,38 @@ namespace Led.Utility
             PositionInBus = positionInBus;
             Led = led;
         }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            if ((obj as LedModelID).BusID == BusID && (obj as LedModelID).PositionInBus == PositionInBus && (obj as LedModelID).Led == Led)
+                return true;
+            else
+                return false;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            int res = BusID;
+            res = res << 8;
+            res += PositionInBus;
+            res = res << 8;
+            res += Led;
+
+            return res.GetHashCode();
+        }
     }
 }
