5d5877c65fd6d46e2fd860cd153d538ec9eb04a0
bl4ck <bl4ck@posteo.net>
2018-10-22 20:57:22 +0200

    Added Description

--- Led/Model/Second.cs
+++ Led/Model/Second.cs
@@ -6,9 +6,15 @@ namespace Led.Model
     [JsonObject]
     public class Second
     {
+        /// <summary>
+        /// Represents the state changed of each Led in each Frame of this Second.
+        /// </summary>
         [JsonProperty]
         public Frame[] Frames { get; set; }
 
+        /// <summary>
+        /// Represents the state of all Leds at the beginning of this Second.
+        /// </summary>
         [JsonProperty]        
         public List<LedChangeData> LedEntityStatus { get; set; }
         
