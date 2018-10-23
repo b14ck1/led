18404c9947393c097bfe26afa943cf0b69ccd427
bl4ck <bl4ck@posteo.net>
2018-10-23 13:44:12 +0200

    Added Property MaxLeds

--- Led/Model/LedGroup.cs
+++ Led/Model/LedGroup.cs
@@ -37,6 +37,39 @@ namespace Led.Model
         [JsonProperty]
         public LedGroupViewProperty View { get; set; }
 
+        public Point MaxLeds
+        {
+            get
+            {
+                int _maxGridX = View.LedGrid.GetLength(0);
+                int _maxGridY = View.LedGrid.GetLength(1);
+
+                Point _start = new Point(_maxGridX, _maxGridY);
+                Point _end = new Point(0, 0);
+
+                for (int x = 0; x < _maxGridX; x++)
+                {
+                    for (int y = 0; y < _maxGridY; y++)
+                    {
+                        if(View.LedGrid[x,y].Status)
+                        {
+                            if (x < _start.X)
+                                _start.X = x;
+                            if (y < _start.Y)
+                                _start.Y = y;
+
+                            if (x > _end.X)
+                                _end.X = x;
+                            if (y > _end.Y)
+                                _end.Y = y;
+                        }
+                    }
+                }
+
+                return new Point(_end.X - _start.X + 1, _end.Y - _start.Y + 1);
+            }
+        }
+
         public LedGroup()
         {
             PositionInEntity = new Point();
