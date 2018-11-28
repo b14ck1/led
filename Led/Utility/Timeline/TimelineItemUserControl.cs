7d3807a468d7f32ed50a7988a900845db7e0bda8
bl4ck_Laptop <bl4ckl@posteo.de>
2018-11-28 07:09:05 +0100

    Added base classes

---
+++ Led/Utility/Timeline/TimelineItemUserControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Led.Utility.Timeline
+{
+    class TimelineItemUserControl : Button
+    {
+        private ViewModels.EffectBaseVM _EffectBaseVM;
+
+        public TimelineItemUserControl()
+        {
+
+        }
+    }
+}
